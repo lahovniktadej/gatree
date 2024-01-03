a1801f6e9ad9b62f9613923873ee1be6ebb4d870
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2024-01-03 11:19:12 +0100

    doc: Version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Tadej Lahovnik, Sašo Karakatič'
 author = 'Tadej Lahovnik, Sašo Karakatič'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 
