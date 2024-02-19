c43afc4adf3e37f620d06413c77f53eae6147880
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2024-02-19 11:53:22 +0100

    feat: Example (iris)

---
+++ examples/iris.py
@@ -0,0 +1,30 @@
+"""
+The following example shows how to perform classification 
+of the iris dataset using GATree
+"""
+
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from gatree.gatree import GATree
+
+# Load the iris dataset
+iris = load_iris()
+X = pd.DataFrame(iris.data, columns=iris.feature_names)
+y = pd.Series(iris.target, name='target')
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=123)
+
+# Create and fit the GATree classifier
+gatree = GATree(n_jobs=16, random_state=123)
+gatree.fit(X=X_train, y=y_train, population_size=100, max_iter=100)
+
+# Make predictions on the testing set
+y_pred = gatree.predict(X_test)
+
+# Evaluate the accuracy of the classifier
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
